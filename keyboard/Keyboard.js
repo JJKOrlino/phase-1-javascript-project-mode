a6575c514d2db9d65e666453fe0a943c132dacce
JJKOrlino <jjkorlino80@yahoo.com>
2021-04-10 21:27:51 -0500

    Key Loops

--- keyboard/Keyboard.js
+++ keyboard/Keyboard.js
@@ -29,6 +29,26 @@ const Keyboard = {
     },
 
     createKeys() {
+        const letter = document.createDocumentLetter();
+        const keyLayout = [
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "<-",
+            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+            "capsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ENTER",
+            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+            "spacebar"
+        ];
+
+        const createIcon = (iconName) => {
+            return `<i class="letter-icons">${iconName}</i>`;
+        }
+
+        keyLayout.forEach(key => {
+            const keyElement = document.createElement("button");
+            const lineBreak = ["<-", "p", "ENTER", "?"].indexOf(key) !== -1;
+
+            keyElement.setAttribute("type", "button");
+            keyElement.classList.add("keyboardKey");
+        });
 
     },
 
